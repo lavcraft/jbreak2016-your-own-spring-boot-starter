612c2e055dd5251af13e7dc18aaddebea08d761f
Kirill Tolkachev <lavcraft@gmail.com>
2016-03-18 15:26:39 +0300

    #2 Init Spring App and fix test

--- examples/simple-client/src/main/java/info/developerblog/examples/thirft/simpleclient/SimpleClientApplication.java
+++ examples/simple-client/src/main/java/info/developerblog/examples/thirft/simpleclient/SimpleClientApplication.java
@@ -1,11 +1,58 @@
 package info.developerblog.examples.thirft.simpleclient;
 
+import example.TInvestigatingService;
+import example.TName;
+import org.apache.thrift.TProcessor;
+import org.apache.thrift.server.TServer;
+import org.apache.thrift.server.TSimpleServer;
+import org.apache.thrift.transport.TServerSocket;
+import org.apache.thrift.transport.TServerTransport;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.util.concurrent.TimeUnit;
+
 @SpringBootApplication
 public class SimpleClientApplication {
+  Integer port = 6565;
+  TServer server;
+  Thread thread;
+
+  @PostConstruct
+  public void init() throws InterruptedException {
+    thread = new Thread(() -> {
+      try {
+        TServerTransport serverTransport = new TServerSocket(
+            new InetSocketAddress(InetAddress.getLocalHost(), port));
+        TProcessor processor = new TInvestigatingService.Processor<>(
+            (TName name) -> "Where is " + name.getFirstName() + "?"
+        );
+
+        server = new TSimpleServer(new TServer.Args(serverTransport).processor(processor));
+
+        System.out.println("Starting the simple server...");
+        server.serve();
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    });
+
+    thread.start();
+
+    while (server == null || !server.isServing()) {
+      TimeUnit.MILLISECONDS.sleep(100);
+    }
+  }
   public static void main(String[] args) {
     SpringApplication.run(SimpleClientApplication.class, args);
+  }
+
+  @PreDestroy
+  public void close(){
+    server.stop();
   }
 }
--- examples/simple-client/src/test/java/info/developerblog/examples/thirft/simpleclient/TGreetingServiceHandlerTests.java
+++ examples/simple-client/src/test/java/info/developerblog/examples/thirft/simpleclient/TGreetingServiceHandlerTests.java
@@ -3,51 +3,20 @@ package info.developerblog.examples.thirft.simpleclient;
 import example.TInvestigatingService;
 import example.TName;
 import org.apache.thrift.TException;
-import org.apache.thrift.TProcessor;
 import org.apache.thrift.protocol.TBinaryProtocol;
-import org.apache.thrift.server.TServer;
-import org.apache.thrift.server.TSimpleServer;
-import org.apache.thrift.transport.TServerSocket;
-import org.apache.thrift.transport.TServerTransport;
 import org.apache.thrift.transport.TSocket;
 import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.util.concurrent.TimeUnit;
-
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = {
+    SimpleClientApplication.class
+})
 public class TGreetingServiceHandlerTests {
   Integer port = 6565;
-  TServer server;
-  Thread thread;
-
-  @Before
-  public void setUp() throws Exception {
-    thread = new Thread(() -> {
-      try {
-        TServerTransport serverTransport = new TServerSocket(
-            new InetSocketAddress(InetAddress.getLocalHost(), port));
-        TProcessor processor = new TInvestigatingService.Processor<>(
-            (TName name) -> "Where is " + name.getFirstName() + "?"
-        );
-
-        server = new TSimpleServer(new TServer.Args(serverTransport).processor(processor));
-
-        System.out.println("Starting the simple server...");
-        server.serve();
-      } catch (Exception e) {
-        e.printStackTrace();
-      }
-    });
-
-    thread.start();
-
-    while (server == null || !server.isServing()) {
-      TimeUnit.MILLISECONDS.sleep(100);
-    }
-  }
 
   @Test
   public void testSimpleCall() throws Exception {
@@ -72,6 +41,5 @@ public class TGreetingServiceHandlerTests {
 
   @After
   public void clean() {
-    server.stop();
   }
 }
